a780fc5f56dd1e0de82dedcb1c2151961fa6db93
Rohitprasad95albert <rkprasad90600@gmail.com>
2025-07-26 11:03:25 +0530

    added authentication and authorization

--- models/person.js
+++ models/person.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-
+const bcrypt = require('bcrypt');
 // define the Person schema
 const personSchema =new mongoose.Schema({
     name: {
@@ -28,10 +28,49 @@ const personSchema =new mongoose.Schema({
     salary: {
         type: Number,
         required: true
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
     }
 });
 
+personSchema.pre('save',async function(next){
+    const person = this;
 
+    //Hash the password only if it has been modified(or is new)
+    if(!person.isModified('password')) return next();
+
+    try{
+        //hash password generation
+        const salt = await bcrypt.genSalt(10);
+
+        // hash password
+        const hashedPassword = await bcrypt.hash(person.password,salt);
+
+        // Override the plain passwoord with the hashed one
+        person.password = hashedPassword;
+        next();
+    }catch(err){
+        return next(err);
+    }
+})
+
+personSchema.methods.comparePassword = async function(candidatePassword){
+    try{
+        // Use bcrypt to comapre the provided password with the hashed password
+        const isMatch = await bcrypt.compare(candidatePassword,this.password);
+        return isMatch;
+
+    }catch(err){
+        throw err;
+    }
+    
+}
 // Create person Model
 
 const Person = mongoose.model('Person', personSchema);
--- server.js
+++ server.js
@@ -2,17 +2,28 @@ const express = require('express')
 const app = express();
 const db = require('./db');
 require('dotenv').config();
+const passport = require('./auth');
+
 const bodyParser = require('body-parser');
 app.use(bodyParser.json()); //req.body
+const PORT = process.env.PORT || 3000;
 
 
 
 
+//  Middleware Function
+const logrequest = (req,res,next) => {
+console.log(`[${new Date().toLocaleString()}] Request Made to : ${req.originalUrl}`);
+next(); // move to next phase
+}
+app.use(logrequest);      //using this all router logrequest implement, kuch aisa hi.
 
 
-const port = 3000
 
-app.get('/', (req, res) => {
+app.use(passport.initialize());  
+
+const localAuthMiddleware = passport.authenticate('local',{session: false})
+app.get('/',function (req, res)  {
   res.send('Welcome to my hotel')
 })
 
@@ -30,10 +41,12 @@ const personRoutes = require('./routes/personRoutes');
 const MenuItemRoutes = require('./routes/MenuItemRoutes')
 
 // Use the routers
-app.use('/person',personRoutes);
+app.use('/person',localAuthMiddleware,personRoutes);
 app.use('/menu',MenuItemRoutes);
 
-const PORT = process.env.PORT || 3000;
+const port = 3000
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
